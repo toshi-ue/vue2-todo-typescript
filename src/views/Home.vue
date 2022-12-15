e7b4dadf2189832e93bd8ce2f691cd8ebcc5796a
toshi-ue <t.ueno.dev+githubtoshiue@gmail.com>
2022-12-15 20:40:57 +0900

    Todoを追加

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,101 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+  <div>
+    <v-text-field
+      v-model="newTaskTitle"
+      class="mt-3 ml-3"
+      label="Add Task"
+      @keyup.enter="addTask"
+      clearable
+      hide-details
+      counter
+      maxlength="20"
+      prepend-icon="mdi-plus"
+    ></v-text-field>
+    <v-list>
+      <!-- FIXME: なぜtemplateは使えないのか?
+              divでtasksをループで出力する。templateは使えない。
+      -->
+      <div v-for="task in tasks" :key="task.id">
+        <!-- クリックしたら、背景色を変更し、tasksを更新するイベントを追加 -->
+        <v-list-item @click="doneTask(task.id)" :class="{ 'blue lighten-5': task.done }">
+          <template>
+            <!-- タイトル、チェックボックスをtasksと連動させる -->
+            <v-list-item-action>
+              <v-checkbox :input-value="task.done"></v-checkbox>
+            </v-list-item-action>
+
+            <v-list-item-content>
+              <!-- タスクが完了したら取り消し線にする。 -->
+              <v-list-item-title :class="{ 'text-decoration-line-through': task.done }">{{
+                task.title
+              }}</v-list-item-title>
+            </v-list-item-content>
+
+            <!-- deleteアイコンに変更し、tasksを削除するイベント追加 -->
+            <v-list-item-icon>
+              <v-icon color="primary" @click="deleteTask(task.id)">mdi-delete</v-icon>
+            </v-list-item-icon>
+          </template>
+        </v-list-item>
+        <v-divider></v-divider>
+      </div>
+    </v-list>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
-import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
 
 export default Vue.extend({
   name: "Home",
-  components: {
-    HelloWorld,
+  data: () => ({
+    newTaskTitle: "",
+    tasks: [
+      {
+        id: 1,
+        title: "起きる",
+        done: false,
+      },
+      {
+        id: 2,
+        title: "着替える",
+        done: false,
+      },
+    ],
+  }),
+  methods: {
+    // タスクを検索、フラグの更新
+    doneTask(id: number) {
+      // // taskを検索し、フラグを更新する。
+      // FIXME: 以下の書き方がわからない
+      // this.tasks.find((t) => t.id === id)
+      let task = this.tasks.find((t) => t.id === id);
+      if (task !== undefined) {
+        task.done = !task.done;
+      }
+    },
+    // タスクを削除
+    deleteTask(id: number) {
+      this.tasks.forEach((task, index) => {
+        if (task.id == id) {
+          this.tasks.splice(index, 1);
+        }
+      });
+      // 以下の書き方もできるが、部品化すると更新できなくなるので却下
+      // this.tasks = this.tasks.filter((t) => t.id !== id);
+    },
+    // taskを追加
+    addTask() {
+      if (this.newTaskTitle.length > 0) {
+        let newTask = {
+          id: Date.now(),
+          title: this.newTaskTitle,
+          done: false,
+        };
+        this.tasks.push(newTask);
+        this.newTaskTitle = "";
+      }
+    },
   },
 });
 </script>
